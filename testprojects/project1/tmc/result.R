b210b86de8689b88ce8c2aaec72a0f1ea3808ab2
anttihaap <antti.haapaniemi@helsinki.fi>
2017-09-08 10:42:33 +0300

    Added an example of reading testthat results.

--- testprojects/project1/tmc/result.R
+++ testprojects/project1/tmc/result.R
@@ -1,4 +1,21 @@
 library('testthat')
 
-output <- test_dir('tests/testthat/', reporter="silent")
-print(output)
+testthat_output <- test_dir('tests/testthat/', reporter="silent")
+
+for (test in testthat_output) {
+  test_failed <- FALSE
+  test_failures <- c()
+  for (result in test$results) {
+    if (format(result) != "As expected") {
+      test_failed <- TRUE
+      test_failures <- c(test_failures, format(result))
+    }
+  }
+  
+  if (test_failed) {
+    print(paste(test$test, ": FAIL", sep = ""))
+    print(paste("   ", test_failures, sep = ""))
+  } else {
+    print(paste(test$test, ": PASS", sep = ""))
+  }
+}
