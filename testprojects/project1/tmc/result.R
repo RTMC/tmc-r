c94e760ffe0359e657dac79fa26652d862b50e7f
aleksisv <aleksis.vuoksenmaa@helsinki.fi>
2017-09-09 12:11:47 +0300

    results are bound in a data frame and read to json.

--- testprojects/project1/tmc/result.R
+++ testprojects/project1/tmc/result.R
@@ -1,6 +1,13 @@
 library('testthat')
+library('jsonlite')
 
 testthat_output <- test_dir('tests/testthat/', reporter="silent")
+
+#Create new dataframe with one empty row and five columns.
+results.frame = as.data.frame(matrix(ncol=5, nrow=1))
+
+#Names of the columns
+names(results.frame) = c("backtrace", "status", "name", "message", "points")
 
 for (test in testthat_output) {
   test_failed <- FALSE
@@ -11,11 +18,18 @@ for (test in testthat_output) {
       test_failures <- c(test_failures, format(result))
     }
   }
-  
+
   if (test_failed) {
     print(paste(test$test, ": FAIL", sep = ""))
     print(paste("   ", test_failures, sep = ""))
   } else {
     print(paste(test$test, ": PASS", sep = ""))
+    results.frame = rbind(results.frame, c("[]", "passed", format(test$test), "läpi meni", "1"))
   }
 }
+
+#This is a hack. For some reason there are factor-problems constructing the data frame is not done as follows.
+results.frame <- results.frame[-1,]
+
+json <-toJSON(results.frame, pretty = TRUE)
+write_json(json, "results.json")
