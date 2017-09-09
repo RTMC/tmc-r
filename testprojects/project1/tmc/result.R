93a96f6c4c7010b7ab26248d17fa2655a0e40d9c
aleksisv <aleksis.vuoksenmaa@helsinki.fi>
2017-09-09 22:06:03 +0300

    Modified the step where JSON-file is written so that the output is neater and can be read by

--- testprojects/project1/tmc/result.R
+++ testprojects/project1/tmc/result.R
@@ -20,13 +20,15 @@ for (test in testthat_output) {
 
     #Contains an array of the points associated with this test.
     points_assoc <- test_description[[1]][2]
-    
-    print(paste(test_description[[1]][1], ": FAIL", sep = ""))
+    #Contains the name of the test
+    test_name <- test_description[[1]][1]
+
+    print(paste(test_name, ": FAIL", sep = ""))
     print(paste("   ", test_failures, sep = ""))
 
     test_result <- list(backtrace=list(),
         status=unbox("failed"),
-        name=unbox(format(test_description[[1]][1])),
+        name=unbox(format(test_name)),
         message=unbox(""),
         points=as.list(points_assoc))
   } else {
@@ -34,11 +36,13 @@ for (test in testthat_output) {
 
     #Contains an array of the points associated with this test.
     points_assoc <- test_description[[1]][2]
+    #Contains the name of the test
+    test_name <- test_description[[1]][1]
 
-    print(paste(test_description[[1]][1], ": PASS", sep = ""))
+    print(paste(test_name, ": PASS", sep = ""))
     test_result <- list(backtrace=list(),
         status=unbox("passed"),
-        name=unbox(format(test_description[[1]][1])),
+        name=unbox(format(test_name)),
         message=unbox(""),
         points=as.list(points_assoc))
   }
@@ -46,6 +50,8 @@ for (test in testthat_output) {
 }
 
 #json utf-8 coded:
-json <- enc2utf8(toJSON(results, pretty = TRUE))
+json <- enc2utf8(toJSON(results, pretty = FALSE))
+json <- prettify(json)
+
 #encode json to utf-8 and write file
-write_json(json, ".results.json")
+write(json, ".results.json")
