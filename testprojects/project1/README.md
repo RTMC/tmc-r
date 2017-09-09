a29ff78416c976992389bf2b02a4fcbecf18fcd5
aleksisv <aleksis.vuoksenmaa@helsinki.fi>
2017-09-09 18:42:43 +0300

    Made result.R aesthetically more pleasing. Added a README-file to the test project.

---
+++ testprojects/project1/README.md
@@ -0,0 +1,1 @@
+To run tests on this test project, run the script `tmc_run_test_example.sh`.
--- testprojects/project1/tmc/result.R
+++ testprojects/project1/tmc/result.R
@@ -20,23 +20,28 @@ for (test in testthat_output) {
 
     #Contains an array of the points associated with this test.
     points_assoc <- test_description[[1]][2]
-
+    
     print(paste(test_description[[1]][1], ": FAIL", sep = ""))
     print(paste("   ", test_failures, sep = ""))
 
-    test_result <- list(backtrace=list(), status=unbox("failed"), name=unbox(format(test_description[[1]][1])), message=unbox(""), points=as.list(points_assoc))
+    test_result <- list(backtrace=list(),
+        status=unbox("failed"),
+        name=unbox(format(test_description[[1]][1])),
+        message=unbox(""),
+        points=as.list(points_assoc))
   } else {
-
     test_description <- strsplit(format(test$test), "#")
 
     #Contains an array of the points associated with this test.
     points_assoc <- test_description[[1]][2]
 
     print(paste(test_description[[1]][1], ": PASS", sep = ""))
-    test_result <- list(backtrace=list(), status=unbox("passed"), name=unbox(format(test_description[[1]][1])), message=unbox(""), points=as.list(points_assoc))
+    test_result <- list(backtrace=list(),
+        status=unbox("passed"),
+        name=unbox(format(test_description[[1]][1])),
+        message=unbox(""),
+        points=as.list(points_assoc))
   }
-
-
   results <- c(results, test_result)
 }
 
