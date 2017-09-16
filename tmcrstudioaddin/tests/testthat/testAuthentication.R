c4ea9dcefd91bf3056e0d31b732707aec80e694e
Tuomo Lehtonen <tuomo.lehtonen@cs.helsinki.fi>
2017-09-16 15:50:03 +0300

    simple tests for authentication

---
+++ tmcrstudioaddin/tests/testthat/testAuthentication.R
@@ -0,0 +1,10 @@
+source('../../R/Authentication.R')
+
+test_that("Authentication gives a token", {
+  token <- authenticate("rtest", "asdasdasd")
+  expect_is(token, "character")
+})
+
+test_that("False authentication fails", {
+  expect_is(authenticate("sddsdaddsdsdadasads", "dsdasasdsdsdasdasdasd"), "list")
+})
