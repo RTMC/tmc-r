fb04899f16bb1b9978f3824b8546fef38b5a470a
aleksisv <aleksis.vuoksenmaa@helsinki.fi>
2017-09-17 14:48:04 +0300

    Merged two branches

--- example_projects/example_project1/tmc/result.R
+++ example_projects/example_project1/tmc/result.R
@@ -10,27 +10,13 @@ testthat_output <- test_file('tests/testthat/testArithmetics.R', reporter="silen
 
 results = list()
 
-#Compiles the list of points from the test description
-points_compiler <- function(desc) {
-    points_assoc <- desc[[1]][2]
-    points_assoc <- gsub("\\[|\\]", "", points_assoc)
-    points_assoc <- gsub(",", "", points_assoc)
-    points_assoc <- as.list(lapply(strsplit(points_assoc, '\\s+')[[1]], unbox))
-}
-
-#We go through the test output
 for (test in testthat_output) {
   test_name <- test$test
   test_points <- points[[test_name]]
-  
+
   test_failed <- FALSE
   test_failures <- c()
-<<<<<<< HEAD
-  
-=======
 
-  #For single tests, we mark whether it passed or failed.
->>>>>>> real-exe
   for (result in test$results) {
     if (format(result) != "As expected") {
       test_failed <- TRUE
@@ -38,15 +24,7 @@ for (test in testthat_output) {
     }
   }
 
-  #If the test failed, we produce a corresponding output
   if (test_failed) {
-<<<<<<< HEAD
-=======
-    test_description <- strsplit(format(test$test), "#")
-    points <- points_compiler(test_description)
-    test_name <- test_description[[1]][1]
-
->>>>>>> real-exe
     print(paste(test_name, ": FAIL", sep = ""))
     print(paste("   ", test_failures, sep = ""))
 
@@ -54,33 +32,15 @@ for (test in testthat_output) {
         status=unbox("failed"),
         name=unbox(format(test_name)),
         message=unbox(""),
-<<<<<<< HEAD
         points=list())
   } else {
-=======
-        points=points)
-
-  }
-  #If the test passed, we produce a corresponding output.
-  else {
-    test_description <- strsplit(format(test$test), "#")
-
-    points <- points_compiler(test_description)
-
-    test_name <- test_description[[1]][1]
-
->>>>>>> real-exe
     print(paste(test_name, ": PASS", sep = ""))
-    
+
     test_result <- list(backtrace=list(),
         status=unbox("passed"),
         name=unbox(format(test_name)),
         message=unbox(""),
-<<<<<<< HEAD
         points=test_points)
-=======
-        points=points)
->>>>>>> real-exe
   }
   results[[length(results)+1]] <- test_result
 }
