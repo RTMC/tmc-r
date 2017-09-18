82e697027abeedf35567d8a5560285c0ef6590b1
Tuomo Lehtonen <tuomo.lehtonen@cs.helsinki.fi>
2017-09-18 10:58:05 +0300

    removed copy-paste code

--- example_projects/example_project1/tmc/result.R
+++ example_projects/example_project1/tmc/result.R
@@ -15,18 +15,18 @@ for (test in testthat_output) {
     }
   }
 
+  test_description <- strsplit(format(test$test), "#")
+  
+  #Contains an array of the points associated with this test.
+  points_assoc <- test_description[[1]][2]
+  points_assoc <- gsub("\\[|\\]", "", points_assoc)
+  points_assoc <- gsub(",", "", points_assoc)
+  points_assoc <- as.list(lapply(strsplit(points_assoc, '\\s+')[[1]], unbox))
+  
+  #Contains the name of the test
+  test_name <- test_description[[1]][1]
+  
   if (test_failed) {
-    test_description <- strsplit(format(test$test), "#")
-
-    #Contains an array of the points associated with this test.
-    points_assoc <- test_description[[1]][2]
-    points_assoc <- gsub("\\[|\\]", "", points_assoc)
-    points_assoc <- gsub(",", "", points_assoc)
-    points_assoc <- as.list(lapply(strsplit(points_assoc, '\\s+')[[1]], unbox))
-
-    #Contains the name of the test
-    test_name <- test_description[[1]][1]
-
     print(paste(test_name, ": FAIL", sep = ""))
     print(paste("   ", test_failures, sep = ""))
 
@@ -36,18 +36,6 @@ for (test in testthat_output) {
         message=unbox(""),
         points=points_assoc)
   } else {
-    test_description <- strsplit(format(test$test), "#")
-
-    #Contains an array of the points associated with this test.
-    points_assoc <- test_description[[1]][2]
-    points_assoc <- gsub("\\[|\\]", "", points_assoc)
-    points_assoc <- gsub(",", "", points_assoc)
-    points_assoc <- as.list(lapply(strsplit(points_assoc, '\\s+')[[1]], unbox))
-
-
-    #Contains the name of the test
-    test_name <- test_description[[1]][1]
-
     print(paste(test_name, ": PASS", sep = ""))
     test_result <- list(backtrace=list(),
         status=unbox("passed"),
