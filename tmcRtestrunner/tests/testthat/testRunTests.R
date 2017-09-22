ec424e186f0ade2f1162a5cb78054b0ab322fe4b
Aleksis Vuoksenmaa <aleksis.vuoksenmaa@helsinki.fi>
2017-09-22 13:14:39 +0300

    Started writing tests to correct functions.

--- tmcRtestrunner/tests/testthat/testRunTests.R
+++ tmcRtestrunner/tests/testthat/testRunTests.R
@@ -1,3 +1,9 @@
+library('jsonlite')
+
+path <- file.path(getwd())
+path <- dirname(dirname(dirname(path)))
+path_to_example1 <- paste(sep = "", path, "/example_projects/example_project1")
+path_to_json <- paste(set="", path_to_example1, "/.results.json")
 test_that("Dummy test before real tests.", {
   expect_true(DummyFunction())
 })
@@ -12,4 +18,8 @@ test_that("Dummy3 works", {
 
 test_that("Hidden functions work", {
   expect_true(.hidden_function())
+})
+
+test_that("Tests pass", {
+  expect_true(T)
 })
